64f8bf08e63a41fb0e7ff9f2b294377ee5ec3e42
Myduzo <sahli.youssef@outlook.com>
2020-08-31 15:07:05 +0100

    python + text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """method returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """method returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    """method returns C + text"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is_cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """method returns python + text"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
