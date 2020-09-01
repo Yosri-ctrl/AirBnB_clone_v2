607644b39278eb8ac6745edcea3a394db1df7ca2
Myduzo <sahli.youssef@outlook.com>
2020-09-01 13:52:53 +0100

    odd or even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """method returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """method returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    """method returns C + text"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is_cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """method returns python + text"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    """method returns number"""
+    return '{} is a number'.format(int(n))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def html_number(n):
+    """method returns html script"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even(n):
+    """method returns odd or even"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
