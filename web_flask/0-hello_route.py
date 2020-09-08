78f734ddadf7afa1d88b4ce1f67a3fa02b5b5421
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-08 23:03:26 +0100

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
-
+"""Start a flak application"""
 from flask import Flask
+
+
 app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
+@app.route("/")
 def hello():
-    """method returns Hello HBNB!"""
-    return 'Hello HBNB!'
+    """print Hello HBNB"""
+    return ("Hello HBNB!")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run()
