03e08b780fccc0892f4bf97416cc90a7bbc02e1a
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-08 23:40:27 +0100

    Task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Start a flak application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    """print Hello HBNB"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb")
+def hello_HBNB():
+    """print HBNB"""
+    return ("HBNB!")
+
+
+if __name__ == "__main__":
+    app.run()
